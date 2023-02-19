d12380a5a18d75d996d84b3db1095d06e8324c4f
YOSHIKI-NAGAMINE <y.nagamine1236@gmail.com>
2023-02-19 18:31:59 +0900

    画面遷移の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,4 +1,8 @@
 class RoomsController < ApplicationController
+  
+  def index
+  end
+
 
   def new
     @room = Room.new
@@ -14,8 +18,8 @@ class RoomsController < ApplicationController
   end
 
   private
+
   def room_params
     params.require(:room).permit(:name, user_ids: [])
   end
-
 end
